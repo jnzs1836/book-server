e98b1490cc0229df550e7076f86f85aba31df0fe
jnzs1836 <wuguandejn@gmail.com>
2018-05-12 03:35:47 +0800

    create seeders

---
+++ database/seeds/BookTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class BookTableSeeder extends Seeder{
+    /**
+     * 运行数据库填充
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('books')->insert([
+            'name' => str_random(10),
+            'author' => str_random(10),
+            'press' => str_random(10),
+            'quantity' => 4,
+            'stock' => 4
+        ]);
+    }
+}
