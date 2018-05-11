fc60eaf9ffda96af4ee407f182b1771b5be3047d
jnzs1836 <wuguandejn@gmail.com>
2018-05-11 17:09:26 +0800

    allow cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -59,9 +59,10 @@ $app->singleton(
 |
 */
 
-// $app->middleware([
+ $app->middleware([
+     \App\Http\Middleware\Cors::class,
 //    App\Http\Middleware\ExampleMiddleware::class
-// ]);
+ ]);
 
  $app->routeMiddleware([
      'auth' => App\Http\Middleware\Authenticate::class,
